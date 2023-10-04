93af33a883fc091cab8de494e28d4f524b800830
skyforce2022 <aryateutong@gmail.com>
2023-10-04 19:05:15 +0700

    add program .cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_714220017_Arya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan Angka Pertama");
+            int angka1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Masukkan Angka Kedua");
+                int angka2 = Convert.ToInt32(Console.ReadLine());
+
+            int penjumlahan;
+            int pengurangan;
+            int perkalian;
+            float pembagian;
+
+            penjumlahan = angka1 + angka2;
+            pengurangan = angka1 - angka2;
+            perkalian = angka1 * angka2;
+            pembagian = (float)angka1 / angka2;
+
+            Console.WriteLine($"{angka1} + {angka2} = {penjumlahan}");
+            Console.WriteLine($"{angka1} - {angka2} = {pengurangan}");
+            Console.WriteLine($"{angka1} * {angka2} = {perkalian}");
+            Console.WriteLine($"{angka1} / {angka2} = {pembagian}");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
