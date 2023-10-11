203185b970c3e3d842be76e8387d85fd88c7f3c0
skyforce2022 <aryateutong@gmail.com>
2023-10-11 20:11:07 +0700

    rapihin folder dan add tugas pertemuan 3

---
+++ Pertemuan_3/Praktikum/P3_3_714220017/P3_3_714220017/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_3_714220017
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            Console.Write("MENU PERSEGI PANJANG");
+            Console.Write("\n1. Hitung Luas");
+            Console.Write("\n2. Hitung Keliling");
+
+            Console.Write("\nMenu Pilihan: ");
+
+            int menu = Convert.ToInt16(Console.ReadLine());
+
+            Boolean ulang = true;
+
+            while (ulang)
+            {
+                if (menu == 1)
+                {
+                    Console.Write("MENGHITUNG LUAS PERSEGI PANJANG");
+                    Console.Write("\nMasukkan Panjang: ");
+                    int panjang = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("\nMasukkan Lebar: ");
+                    int lebar = Convert.ToInt16(Console.ReadLine());
+
+                    int luas = panjang * lebar;
+                    Console.WriteLine("Luas persegi panjang {0}", luas);
+                    Console.Write("Ingin mengulang kembali (Y/T)?");
+
+                    String pengulangan = Console.ReadLine();
+                    if (pengulangan == "T")
+                    {
+                        ulang = false;
+                    }
+                }
+                else if (menu == 2)
+                {
+                    Console.Write("MENGHITUNG KELILING PERSEGI PANJANG");
+                    Console.Write("\nMasukkan Panjang: ");
+                    int panjang = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("\nMasukkan Lebar: ");
+                    int lebar = Convert.ToInt16(Console.ReadLine());
+
+                    int keliling = 2 * (panjang + lebar);
+                    Console.WriteLine("Keliling persegi panjang {0}", keliling);
+
+                    Console.Write("Ingin mengulang kembali (Y/T)?");
+                    String pengulangan =
+                        Console.ReadLine();
+
+                    if (pengulangan == "T")
+                    {
+                        ulang = false;
+                    }
+                }
+                else
+                {
+                    Console.Write("Menu Tidak Tersedia");
+                    ulang = false;
+                }
+            }
+        }
+    }
+}
---
+++ Pertemuan_3/Praktikum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P3_2_714220017
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            Console.Write("PROGRAM SYAIR LAGU ANAK AYAM\n------------------------------");
+            Console.Write("\nMasukkan jumlah anak ayam: ");
+
+            int jumlah = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("ANAK AYAM TURUN {0}", jumlah);
+
+            if (jumlah < 1 || jumlah > 10)
+            {
+                Console.Write("Nilai tidak boleh dibawah 0 atau diatas 10");
+            }
+            else
+            {
+                for (int i = jumlah; i >= 1; i--)
+                {
+                    if (i - 1 == 0)
+                    {
+                        Console.WriteLine("Anak ayam turunlah {0}, mati satu tinggallah induknya", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Anak ayam turunlah {0}, mati satu tinggallah {1}", i, i - 1);
+
+                    }
+                }
+            }
+        }
+    }
+}
