89f25fb906e151db73b805d6efbe47739c5b393a
skyforce2022 <aryateutong@gmail.com>
2023-10-20 14:07:39 +0700

    rapihin folder dan add P4_1_714220017 ke folder Praktikum dalam folder Pertemuan_4

---
+++ Pertemuan_4/Praktikum/P4_1_714220017/P4_1_714220017/DVD_714220017.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220017
+{
+    public class DVD_714220017 : Product_714220017
+    {
+        protected string _duration;
+
+        public DVD_714220017(string title, string duration)
+        {
+            this.MyTitle = title;
+            this.MyType = "DVD";
+            this._duration = duration;
+
+            Console.WriteLine("DVD constructor called");
+        }
+
+        public string Duration
+        {
+            get
+            {
+                return _duration;
+            }
+
+            set
+            {
+                _duration = value;
+            }
+        }
+    }
+}
--- Pertemuan_4/Praktikum/P4_1_714220017/P4_1_714220017/ProductTest_714220017.cs
+++ Pertemuan_4/Praktikum/P4_1_714220017/P4_1_714220017/ProductTest_714220017.cs
@@ -10,10 +10,11 @@ namespace P4_1_714220017
     {
         static void Main(string[] args)
         {
-            Book_714220017 myBook = new Book_714220017("Book", "C# Object Oriented Solution", "300");
+            Book_714220017 product1 = new Book_714220017("Book", "C# Object Oriented Solution", "300");
+            DVD_714220017 product2 = new DVD_714220017("Ethernal Sunshine Of The Spotless Mind", "145");
 
-
-            Console.WriteLine("\" the book {0} has {1} pages\"", myBook.MyTitle, myBook.PageCount);
+            Console.WriteLine("Product 1 is a {0} called \"{1}\" and has {2} pages", product1.MyType, product1.PageCount);
+            Console.WriteLine("Product 2 is a {0} called \"{1}\" and has {2} minutes duration", product2.MyType,product2.MyTitle, product2.Duration);
         }
     }
 }
